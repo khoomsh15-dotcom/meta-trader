2e122262489fd262534ca32270a9d6f0f4ece3aa
khoomsh15-dotcom <khoomsh15@gmail.com>
2025-12-29 16:32:07 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
-from pymongo import MongoClient
 import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
-# Connect to your MongoDB Atlas
-client = MongoClient("YOUR_MONGODB_URI")
+load_dotenv()
+client = MongoClient(os.getenv("MONGODB_URI"))
 db = client['TradingBotDB']
 users_col = db['users']
 
@@ -16,11 +17,11 @@ def save_creds(user_id, login, password, server):
         upsert=True
     )
 
-def update_settings(user_id, asset, lot, streak):
+def update_trade_settings(user_id, asset, lot, streak):
     users_col.update_one(
         {"user_id": user_id},
         {"$set": {"active_asset": asset, "base_lot": lot, "max_streak": streak, "is_running": True}}
     )
 
-def reset_user(user_id):
+def clear_user(user_id):
     users_col.delete_one({"user_id": user_id})
