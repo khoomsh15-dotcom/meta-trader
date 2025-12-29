c8e7d68e3092c783585a68753f28caced2da0afa
khoomsh15-dotcom <khoomsh15@gmail.com>
2025-12-29 22:09:27 +0530

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,7 +1,6 @@
 import os
 import logging
-import asyncio
-from dotenv import load_dotenv
+import asyncio import load_dotenv
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 from mt5linux import MetaTrader5
